2e6325d2774980b2541b1f7dbef97ceb2341fd5b
Luis Noriega <lnoriegan3w@gmail.com>
2023-11-22 00:32:02 -0700

    notes and adding menus

--- main.py
+++ main.py
@@ -1,29 +1,18 @@
-import mysql.connector
-
-mydb = mysql.connector.connect( # Parametros que necesitamos para la conexion a la base de datos
-    host='localhost',
-    user='root',
-    password='',
-    port='3306',
-    database='distribuidoratelcel'
-)
-
-mycursor = mydb.cursor() # Es el objeto para ejecutar las statements y comunicarse con mysql
-
-mycursor.execute("Select * from productos") # Instruccion a realizar
-
-productos = mycursor.fetchall() # Devuelve una lista de tuples de nuestra instruccion
-
-for producto in productos:
-    print(producto)
-
-
 import tkinter
 
 window = tkinter.Tk()
 
 window.title("Punto de venta") # Title of the window
-window.minsize(width=300, height=300) # Minimum size of the window
+window.minsize(width=1280, height=720) # Minimum size of the window
 window.config(padx=20, pady=20) #Este es el margen o la sangria
 
-window.mainloop()
+main_menu = tkinter.Menu(window) # Menu principal, es la linea sin nada
+window.config(menu=main_menu) # Asignamos como menu principal a la ventana
+
+menu1 = tkinter.Menu(main_menu, tearoff=0) # Creamos el primer menu que tendra menuitems
+
+main_menu.add_cascade(label="archivo", menu=menu1) # Al menu principal le asignamos los menus
+
+menu1.add_command(label="Nuevo") # Son los menuitems, pueden tener funciones
+
+window.mainloop() # Permite que la ventana no se cierre.
