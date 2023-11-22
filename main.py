3e54c51dd311b93de81317b9c4f224efaf99d805
Luis Noriega <lnoriegan3w@gmail.com>
2023-11-22 00:05:28 -0700

    notes and instruction with sql

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import mysql.connector
 
+mydb = mysql.connector.connect( # Parametros que necesitamos para la conexion a la base de datos
+    host='localhost',
+    user='root',
+    password='',
+    port='3306',
+    database='distribuidoratelcel'
+)
+
+mycursor = mydb.cursor() # Es el objeto para ejecutar las statements y comunicarse con mysql
+
+mycursor.execute("Select * from productos") # Instruccion a realizar
+
+productos = mycursor.fetchall() # Devuelve una lista de tuples de nuestra instruccion
+
+for producto in productos:
+    print(producto)
+
+
 import tkinter
 
 window = tkinter.Tk()
