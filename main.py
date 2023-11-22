23e4169cd11d7facd2dba613d1c59f6a8c9f69ec
Luis Noriega <lnoriegan3w@gmail.com>
2023-11-21 23:10:16 -0700

    adding mysql driver plus some windows

---
+++ main.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+import tkinter
+
+window = tkinter.Tk()
+
+window.title("Punto de venta") # Title of the window
+window.minsize(width=300, height=300) # Minimum size of the window
+window.config(padx=20, pady=20) #Este es el margen o la sangria
+
+window.mainloop()
