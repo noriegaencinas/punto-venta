d6938c4852d91f805287f6001d8b869706ed8f15
Luis Noriega <lnoriegan3w@gmail.com>
2023-11-27 00:54:21 -0700

    Implementació: acerca de

--- empresa.py
+++ empresa.py
@@ -2,19 +2,22 @@ from PIL import Image
 from ventana_general import *
 import customtkinter
 
-MENU_IMAGE_WIDTH = 50
-MENU_IMAGE_HEIGHT = 50
-MENU_BUTTON_WIDTH = 104
-MENU_BUTTON_HEIGHT = 80
-
 class Empresa(SubVentana):
     def __init__(self, VentanaBase:object, ventana_dimension:str, titulo_ventana:str):
         super().__init__(VentanaBase, ventana_dimension, titulo_ventana)
 
+        # Dimensiones de la ventana
+        win_width = int(ventana_dimension.split("x")[0])
+        # win_height = int(ventana_dimension.split("x")[1])
+
+        menu_frame = customtkinter.CTkFrame(master=self.new_window, width=win_width, height=100)
+        menu_frame.grid()
 
 
 
 
 # Para testear el codigo
 if __name__ == '__main__':
-    test = Empresa()
+    test = Ventana("400x600", "Empresa")
+    test.testSizes()
+    test.window.mainloop()
