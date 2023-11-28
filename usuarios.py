c388def186697a61a1d107997738ab7059d3f29d
josea <joseale282003@gmail.com>
2023-11-28 14:00:50 -0700

    Implementació: usuarios

--- usuarios.py
+++ usuarios.py
@@ -1,17 +1,24 @@
-from tkinter import messagebox
 from customtkinter import CTkLabel, CTkButton
+from tkinter import messagebox
 import mysql.connector
 import tkinter as tk
+from PIL import Image, ImageTk
+import customtkinter
 
 class Usuarios(tk.Tk):
-    def __init__(self):
+    def __init__(self, previous_position=None):
         super().__init__()
-        self.geometry("500x500")
+        self.geometry("800x400")
         self.title("Usuarios")
+
+        # Restaura la posición anterior si está disponible
+        if previous_position:
+            self.geometry(f"+{previous_position[0]}+{previous_position[1]}")
+
         self.my_conn = mysql.connector.connect(
             host='localhost',
             user='root',
-            password='root123!',
+            password='',
             port='3306',
             database='distribuidor')
         self.configure(bg="black")
@@ -19,19 +26,48 @@ class Usuarios(tk.Tk):
 
     def display(self):
         my_cursor = self.my_conn.cursor()
-        my_cursor.execute("SELECT * FROM Empleados LIMIT 10")
+        my_cursor.execute("SELECT * FROM Empleados")
         global i
         i = 0
-        for empleado in my_cursor:
-            for j in range(len(empleado)):
-                # Cambia el color del texto al crear la etiqueta
-                e = CTkLabel(self, width=15, text=str(empleado[j]), anchor="w", fg_color="black")
-                e.grid(row=i, column=j)
 
-            e = CTkButton(self, width=5, text='Edit', anchor="w",
-                          command=lambda k=empleado: self.edit_data(k[0]))
-            e.grid(row=i, column=5)
-            i = i + 1
+        # Nombres de las columnas
+        labels = ['UsuarioID', 'Nombre', 'Apellido', 'NombreUsuario', 'Contraseña']
+        for j, label_text in enumerate(labels):
+            label = CTkLabel(self, width=15, text=label_text, anchor="w", fg_color="black", font=("Arial", 10, "bold"))
+            label.grid(row=i, column=j, padx=10)
+
+        i += 1
+
+        for empleado in my_cursor:
+            # Columna UsuarioID
+            e_id = CTkLabel(self, width=10, text=str(empleado[0]), anchor="w", fg_color="black")
+            e_id.grid(row=i, column=0, padx=10)
+
+            # Columna Nombre
+            e_nombre = CTkLabel(self, width=15, text=str(empleado[1]), anchor="w", fg_color="black")
+            e_nombre.grid(row=i, column=1, padx=10)
+
+            # Columna Apellido
+            e_apellido = CTkLabel(self, width=15, text=str(empleado[2]), anchor="w", fg_color="black")
+            e_apellido.grid(row=i, column=2, padx=10)
+
+            # Columna NombreUsuario
+            e_usuario = CTkLabel(self, width=15, text=str(empleado[3]), anchor="w", fg_color="black")
+            e_usuario.grid(row=i, column=3, padx=10)
+
+            # Columna Contraseña
+            e_contrasena = CTkLabel(self, width=15, text=str(empleado[4]), anchor="w", fg_color="black")
+            e_contrasena.grid(row=i, column=4, padx=10)
+
+            # Botón Editar
+            e_editar = CTkButton(self, width=5, text='Editar', anchor="w", command=lambda k=empleado[0]: self.edit_data(k))
+            e_editar.grid(row=i, column=5, padx=10)
+
+            # Botón Eliminar
+            e_eliminar = CTkButton(self, width=5, text='Eliminar', anchor="w", command=lambda k=empleado[0]: self.delete_data(k))
+            e_eliminar.grid(row=i, column=6, padx=10)
+
+            i += 1
 
     def edit_data(self, id):
         global i
@@ -51,20 +87,47 @@ class Usuarios(tk.Tk):
         self.e4_str_NombreUsuario.set(s[3])
         self.e5_str_Contrasena.set(s[4])
 
+        # Columna UsuarioID
         e1 = tk.Entry(self, textvariable=self.e1_int_UsuarioID, width=10, state='disabled')
-        e1.grid(row=i, column=0)
+        e1.grid(row=i, column=0, padx=10)
+
+        # Columna Nombre
         e2 = tk.Entry(self, textvariable=self.e2_str_Nombre, width=15)
-        e2.grid(row=i, column=1)
+        e2.grid(row=i, column=1, padx=10)
+
+        # Columna Apellido
         e3 = tk.Entry(self, textvariable=self.e3_str_Apellido, width=15)
-        e3.grid(row=i, column=2)
+        e3.grid(row=i, column=2, padx=10)
+
+        # Columna NombreUsuario
         e4 = tk.Entry(self, textvariable=self.e4_str_NombreUsuario, width=15)
-        e4.grid(row=i, column=3)
+        e4.grid(row=i, column=3, padx=10)
+
+        # Columna Contraseña
         e5 = tk.Entry(self, textvariable=self.e5_str_Contrasena, width=15)
-        e5.grid(row=i, column=4)
+        e5.grid(row=i, column=4, padx=10)
 
-        b2 = CTkButton(self, text='Update', command=lambda: self.my_update(), anchor="w", width=5)
-        b2.grid(row=i, column=5)
+        # Botón Actualizar
+        b2 = CTkButton(self, text='Actualizar', command=lambda: self.my_update(), anchor="w", width=5)
+        b2.grid(row=i, column=5, padx=10)
 
+    def delete_data(self, id):
+        if messagebox.askyesno("Eliminar Usuario", "¿Estás seguro de que deseas eliminar este usuario?"):
+            # Almacena las coordenadas antes de cerrar la ventana
+            previous_position = (self.winfo_x(), self.winfo_y())
+
+            delete_cursor = self.my_conn.cursor()
+            delete_cursor.execute("DELETE FROM Empleados WHERE UsuarioID=%s", (id,))
+            self.my_conn.commit()
+            self.after(100, lambda: self.create_new_instance(previous_position))
+
+    def create_new_instance(self, previous_position):
+        # Cierra la ventana anterior
+        self.destroy()
+
+        # Crea una nueva instancia de la clase Usuarios con la posición anterior
+        app = Usuarios(previous_position)
+        app.mainloop()
     def my_update(self):
         data = (
             self.e2_str_Nombre.get(), self.e3_str_Apellido.get(), self.e4_str_NombreUsuario.get(),
